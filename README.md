90a54084ba8b665ea259b52c9a684de029beb5c7
kochiu <26127166@qq.com>
2016-07-05 14:34:42 +0800

    edit rely file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # javaPasser-sdk
 java passer sdk
+
+2016-7-5 this version is new one
---
+++ javaPasser-sdk/src/main/java/com/kochiu/common/Common.java
@@ -0,0 +1,26 @@
+package com.kochiu.common;
+
+/**
+ * Created by gaochao on 16/6/27.
+ */
+public class Common {
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
